ec8b4158bd13f1fd0085cf0bb62e04cf5b96632b
Oscar <oscar.f.larsson@gmail.com>
2020-06-02 19:16:20 +0200

    Serde used for parsing json response

--- src/smhiapi.rs
+++ src/smhiapi.rs
@@ -1,24 +1,30 @@
 extern crate isahc;
+extern crate serde;
 
 use futures::executor::block_on;
 use isahc::prelude::*;
+use serde::{Serialize, Deserialize};
+use serde_json::Result;
 
 static SMHI_BASE_URL: &str = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.json";
 
-pub fn get_weather() -> Result<(), isahc::Error> {
+pub fn get_weather() -> Result<()> {
     block_on(async {
-        let mut response = isahc::get_async(SMHI_BASE_URL).await?;
-        let body = response.text_async().await?;
-        println!("{}", body);
+        let mut response = isahc::get_async(SMHI_BASE_URL).await.unwrap();
+        let body = response.text_async().await.unwrap();
+        // serde.json::to_vec(:<WeatherData>()?; 
+
+        let data: WeatherData = serde_json::from_str(&body).unwrap();
+        println!("{:?}", data);
       
         Ok(())
     })
 }
 
-// #[derive(Serialize, Deserialize, Debug)]
-// struct WeatherData {
-//     updated: i64
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct WeatherData {
+    updated: i64
+}
 
 // impl RestPath<()> for WeatherData {
 //     fn get_path(_: ()) -> Result<String, Error> {
