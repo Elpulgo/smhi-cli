43e114577c5ac21e930bcc2db702dcb01845148c
Oscar <oscar.f.larsson@gmail.com>
2020-06-10 09:57:59 +0200

    Added POC for print

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ extern crate textplots;
 
 pub mod chart;
 pub mod openstreetmap_api;
+pub mod rest_util;
 pub mod smhi_api;
 pub mod url_util;
-pub mod rest_util;
 
 // use chart::print_chart;
 use openstreetmap_api::get_location;
 use smhi_api::get_weather_for;
+use std::iter;
 use std::process;
 
 fn main() {
@@ -40,17 +41,39 @@ fn main() {
 
     match get_weather_for(location.lat, location.lon) {
         Some(we) => {
-            for point in we.points.into_iter(){
-                println!("Ref time: {Ref}\t temp: {Temp}\t wind: {Wind}\t rain: {Rain}\t Visibility: {Vis} \t Desc: {Desc}", 
-                    Ref = point.time, 
+            let mut line_length = 50;
+            let mut first = true;
+
+            println!("Time\t\t\t Temp (°C)\t Wind (ms/s)\t Rain (mm/h)\t Visibility (hm)\t ");
+
+            for point in we.points.into_iter() {
+                let line = format!(
+                    "{Ref}\t {Temp}\t\t {Wind}\t\t {Rain}\t\t {Vis} \t\t {Desc}",
+                    Ref = point.time.format("%Y-%m-%d %H:%M").to_string(),
                     Temp = point.temperature,
                     Wind = point.wind,
                     Rain = point.min_rain,
                     Vis = point.visibility,
                     Desc = point.weather_description
                 );
+                line_length = line.chars().count() * 2 + 3;
+                let divider = iter::repeat("-").take(line_length).collect::<String>();
+                match first {
+                    true => {
+                        println!("{}", String::from(divider));
+                        println!("{}", line);
+                        first = false;
+                    }
+                    false => {
+                        println!("{}", line);
+                    }
+                }
             }
-        },
+            println!(
+                "{}",
+                String::from(iter::repeat("-").take(line_length).collect::<String>())
+            );
+        }
         None => println!("No weatherforecast found for '{}'", location.display_name),
     };
 
