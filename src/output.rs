d0fdfdf98a891d6939185cb65edae78f91158740
Oscar <oscar.f.larsson@gmail.com>
2020-06-11 10:40:26 +0200

    Refactored output.rs. Added print functionalities

--- src/output.rs
+++ src/output.rs
@@ -9,98 +9,88 @@ use serde::{Deserialize, Serialize};
 use smhi_api::{WeatherData, WeatherPoint};
 use std::iter;
 
+const LINE_LENGTH: usize = 75;
+
 pub fn print_weather(weather_data: WeatherData) {
-    let mut line_length = 50;
     let mut first = true;
 
-    let _p = group_per_day(weather_data);
+    let groups = group_points_per_day(weather_data);
+    println!("Time\t Temp (°C)\t Wind (ms/s)\t Rain (mm)\t Visibility (km)\t ");
+    print_divider("=");
 
-    // println!("Time\t\t\t Temp (°C)\t Wind (ms/s)\t Rain (mm/h)\t Visibility (hm)\t ");
+    for group in groups {
+        match first {
+            true => first = false,
+            false => print_divider("-"),
+        }
+        println!("{}", group.day);
+        print_group(group.points);
+    }
+    print_divider("=");
+}
 
-    // for point in weather_data.points.into_iter() {
-    //     let line = format!(
-    //         "{Ref}\t {Temp}\t\t {Wind}\t\t {Rain}\t\t {Vis} \t\t {Desc}",
-    //         // Day = point.time.format("%a %H").to_string(),
-    //         Ref = point.time.format("%Y-%m-%d %H:%M").to_string(),
-    //         Temp = point.temperature,
-    //         Wind = point.wind,
-    //         Rain = point.min_rain,
-    //         Vis = point.visibility,
-    //         Desc = point.weather_description
-    //     );
-    //     line_length = line.chars().count() * 2 + 3;
-    //     let divider = iter::repeat("-").take(line_length).collect::<String>();
-    //     match first {
-    //         true => {
-    //             println!("{}", String::from(divider));
-    //             println!("{}", line);
-    //             first = false;
-    //         }
-    //         false => {
-    //             println!("{}", line);
-    //         }
-    //     }
-    // }
+fn print_group(points: Vec<WeatherPoint>) {
+    for point in points {
+        println!(
+            "{}",
+            format!(
+                "{Ref}\t {Temp}\t\t {Wind}\t\t {Rain}\t\t {Vis} \t\t {Desc}",
+                Ref = point.time.format("%H:%M").to_string(),
+                Temp = point.temperature,
+                Wind = point.wind,
+                Rain = point.min_rain,
+                Vis = point.visibility,
+                Desc = point.weather_description
+            )
+        );
+    }
+}
+
+fn print_divider(divider_symbol: &str) {
     println!(
         "{}",
-        String::from(iter::repeat("-").take(line_length).collect::<String>())
+        String::from(
+            iter::repeat(divider_symbol)
+                .take(LINE_LENGTH)
+                .collect::<String>()
+        )
     );
 }
 
-fn group_per_day(mut weather_data: WeatherData) -> Vec<WeatherDataGroup> {
+fn group_points_per_day(mut weather_data: WeatherData) -> Vec<WeatherDataGroup> {
     let mut groups = Vec::<WeatherDataGroup>::new();
+    let mut datetime = weather_data.points.first_mut().unwrap().time;
+    let mut comparable_key = datetime.format("%m-%d").to_string();
 
-    let mut weekday = weather_data.points.first_mut().unwrap().time;
-    let mut key_comparer = weather_data
-        .points
-        .first_mut()
-        .unwrap()
-        .time
-        .format("%m-%d")
-        .to_string();
-
-    println!("{:?}", weekday);
-
-    let mut found = true;
     let mut remaining_points = weather_data.points;
-    while found == true {
-        match find_for_weekday(&key_comparer, remaining_points.to_vec()) {
+    loop {
+        match find_per_day(&comparable_key, remaining_points.to_vec()) {
             Some((group, remaining)) => {
                 groups.push(group);
-                println!("key comparer AGAIN : {:?}", weekday);
-                weekday = weekday
-                    .checked_add_signed(chrono::Duration::days(1))
-                    .unwrap();
-                key_comparer = weekday.format("%m-%d").to_string();
+                datetime = increment_date_by_days(datetime, 1);
+                comparable_key = datetime.format("%m-%d").to_string();
                 remaining_points = remaining;
             }
-            None => found = false,
+            None => break,
         }
     }
 
-    let seralized = serde_json::to_string(&groups).unwrap();
-    println!("{}", seralized);
     return groups;
 }
 
-fn find_for_weekday(
-    key_comparer: &String,
+fn increment_date_by_days(datetime: DateTime<Utc>, increment: i64) -> DateTime<Utc> {
+    return datetime
+        .checked_add_signed(chrono::Duration::days(increment))
+        .unwrap();
+}
+
+fn find_per_day(
+    comparable_key: &String,
     points: Vec<WeatherPoint>,
 ) -> Option<(WeatherDataGroup, Vec<WeatherPoint>)> {
-    let matched_points: Vec<WeatherPoint> = points
-        .to_vec()
-        .into_iter()
-        .filter(|x| x.time.format("%m-%d").to_string() == key_comparer.to_owned())
-        .collect();
-
-    let non_matched_points: Vec<WeatherPoint> = points
-        .to_vec()
-        .into_iter()
-        .filter(|x| x.time.format("%m-%d").to_string() != key_comparer.to_owned())
-        .collect();
-
-    println!("matched: {:?}", matched_points.len());
-    println!("NON matched: {:?}", non_matched_points.len());
+    let matched_points: Vec<WeatherPoint> = filter_points(points.to_vec(), comparable_key, true);
+    let non_matched_points: Vec<WeatherPoint> =
+        filter_points(points.to_vec(), comparable_key, false);
 
     match matched_points.len() {
         0 => return None,
@@ -108,7 +98,7 @@ fn find_for_weekday(
             return Some((
                 WeatherDataGroup {
                     points: matched_points.to_vec(),
-                    key: key_comparer.to_owned(),
+                    key: comparable_key.to_owned(),
                     day: String::from(
                         matched_points
                             .first()
@@ -120,6 +110,27 @@ fn find_for_weekday(
                 },
                 non_matched_points,
             ))
+        }
+    }
+}
+
+fn filter_points(
+    points: Vec<WeatherPoint>,
+    key: &String,
+    should_match_predicate: bool,
+) -> Vec<WeatherPoint> {
+    let iterable_points = points.into_iter();
+
+    match should_match_predicate {
+        true => {
+            return iterable_points
+                .filter(|x| x.time.format("%m-%d").to_string() == key.to_owned())
+                .collect()
+        }
+        false => {
+            return iterable_points
+                .filter(|x| x.time.format("%m-%d").to_string() != key.to_owned())
+                .collect()
         }
     }
 }
